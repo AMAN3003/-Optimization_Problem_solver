73d4354b46023b11f71519c23ccca2ec52070941
PRINCE RAHEJA <princeraheja143@gmail.com>
2016-04-02 06:26:00 +0000

    _version.py deleted online with Bitbucket

--- _version.py
+++
@@ -1,6 +0,0 @@
-
-version_version = '1.0'
-version_full = 'e4c2e8edac362acab7123654b9e73432'
-def get_versions(default={}, verbose=False):
-    return {'version': version_version, 'full': version_full}
-
