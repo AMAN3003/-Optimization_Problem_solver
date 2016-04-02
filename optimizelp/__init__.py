79d73d93873ea1dec9c37a4535f5b77156ea80d7
PRINCE RAHEJA <princeraheja143@gmail.com>
2016-04-02 06:37:46 +0000

    __init__.py edited online with Bitbucket

--- optimizelp/__init__.py
+++ optimizelp/__init__.py
@@ -10,3 +10,21 @@ log = logging.getLogger(__name__)
 from .util import Available_Solver_lists
 
 Solvers_Presents = Available_Solver_lists()
+
+if Solvers_Presents['GLPK']:
+    from .glpk_interface import Prob_Model, Prob_Variable, Prob_Constraint, Prob_Objective
+elif Solvers_Presents['CPLEX']:
+    from .cplex_interface import Prob_Model, Prob_Variable, Prob_Constraint, Prob_Objective
+else:
+    log.error('no solvers ar present in your system')
+
+if Solvers_Presents['GLPK']:
+    if six.PY3:
+        from . import glpk_interface
+    else:
+        import glpk_interface
+if Solvers_Presents['CPLEX']:
+    if six.PY3:
+        from . import cplex_interface
+    else:
+        import cplex_interface
