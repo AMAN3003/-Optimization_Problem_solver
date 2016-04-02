c448e5ac31728276133333113b7c22c9e7c94d19
PRINCE RAHEJA <princeraheja143@gmail.com>
2016-04-02 06:19:15 +0000

    container.py edited online with Bitbucket

--- optimizelp/container.py
+++ optimizelp/container.py
@@ -20,3 +20,53 @@ class Container(object):
 
     def Re_Indexor(self):
         self.Dict_Val = dict(((Element.name, Element) for Element in self.List_Objects))
+
+    def __len__(self):
+        return len(self.List_Objects)
+
+    def __contains__(self, key):
+        if key in self.Dict_Val:
+            return True
+        elif key in self.List_Objects:
+            return True
+        else:
+            self.Re_Indexor()
+            return key in self.Dict_Val
+
+    def __iter__(self):
+        original_length = len(self.List_Objects)
+        for item in self.List_Objects.__iter__():
+            if original_length != len(self.List_Objects):
+                raise RuntimeError("container changed size during iteration")
+            yield item
+
+    def __getitem__(self, key):
+        try:
+            return self.List_Objects.__getitem__(key)
+        except TypeError:
+            try:
+                return self.Dict_Val[key]
+            except KeyError:
+                self.Re_Indexor()
+                try:
+                    return self.Dict_Val[key]
+                except KeyError:
+                    raise KeyError("%s does not contain an object with name %s" % (self, key))
+
+    def __setitem__(self, key, Var_Value):
+        try:
+            self.has_Name_Attribute(Var_Value)
+            self.List_Objects.__setitem__(key, Var_Value)
+            self.Dict_Val[Var_Value.name] = Var_Value
+        except TypeError:
+            try:
+                item = self.Dict_Val.__getitem__(key)
+                index = self.List_Names.index(item.name)
+                self.Dict_Val[key] = Var_Value
+            except KeyError:
+                self.Re_Indexor()
+                try:
+                    self.Dict_Val[key] = Var_Value
+                except KeyError:
+                    raise KeyError("%s does not contain an object with name %s" % (self, key))
+            self.List_Objects.__setitem__(index, Var_Value)
